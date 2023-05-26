e530b971f2aaaa6610003a067242583739f9b376
Shahadat-Git <noyon560@gmail.com>
2023-05-26 19:56:39 +0600

    Environment variable to firebase config

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -5,15 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAT0UPbaA6kwF6Ozj6n1ZtJHljbJDUFCT4",
-  authDomain: "chill-out-client.firebaseapp.com",
-  projectId: "chill-out-client",
-  storageBucket: "chill-out-client.appspot.com",
-  messagingSenderId: "248602769551",
-  appId: "1:248602769551:web:d02d77cf72d03bb72d98fa"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export default app
